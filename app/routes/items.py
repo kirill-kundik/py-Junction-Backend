2553211f59a6d0042a5bbea085fb4ccca4ada020
TheGreatNectarine <margal.nik@gmail.com>
2019-11-17 01:03:55 +0200

    randomly generate challenges on adding new items

--- app/routes/items.py
+++ app/routes/items.py
@@ -1,8 +1,10 @@
 from connexion import NoContent
 
 import app
-from app.db.models import Item
+import random
+from app.create_challenge.create_challenge import create_challenge_on_item_add
 from app.db.exceptions import DatabaseException
+from app.db.models import Item
 
 
 def get_all(from_user):
@@ -14,6 +16,8 @@ def get_all(from_user):
 def create(item):
     try:
         app.db.add_item(Item(**item))
+        if random.randint(0, 10) < 3:
+            create_challenge_on_item_add(item)
     except DatabaseException:
         return NoContent, 404
     return NoContent, 200
