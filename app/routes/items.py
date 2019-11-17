b9eccc49804d1cc387205ef921b9cc0cab9e3d4d
kyrylo_kundik <goliathusua@gmail.com>
2019-11-17 05:04:07 +0200

    implemented backend part of new challenge announcement, fixed generating descriptions for challenges

--- app/routes/items.py
+++ app/routes/items.py
@@ -15,11 +15,17 @@ def get_all(from_user):
 
 def create(item):
     try:
+
         new_item = Item(**item)
         app.db.add_item(new_item)
         item = app.db.get_item(new_item.id)
+        response = item.dump()
+
         if random.randint(0, 10) < 3:
             create_challenge_on_item_add(item)
+            response.update({"new_challenge_created": True})
+
     except DatabaseException:
         return NoContent, 404
-    return item.dump()
+
+    return response
