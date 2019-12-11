84cf3da869ac33fa72af0486cb3d2990af6ea0f0
Oleksandr Leskiv <oleksandr.leskiv@digia.co>
2019-12-11 12:55:58 +0200

    no-cors policy

--- app/app.py
+++ app/app.py
@@ -12,3 +12,4 @@ flask_app.add_api('swagger.yaml', base_path='/1.0')
 # set the WSGI application callable to allow using uWSGI:
 # uwsgi --http :8080 -w app
 application = flask_app.app
+CORS(application)
